3bc035a1c86d017ed44905f836128e4b082fca05
Jackly1 <jackly@umich.edu>
2026-05-29 15:03:41 -0400

    delete crowded bull

--- my-bike-journey/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import BikeJourney from "@/components/BikeJourney";
-
-export default function Home() {
-  return (
-    <main className="relative w-screen h-screen overflow-hidden bg-black">
-      <BikeJourney />
-    </main>
-  );
-}
